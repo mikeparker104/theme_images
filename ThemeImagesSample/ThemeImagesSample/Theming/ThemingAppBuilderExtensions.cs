929a0088ad9aa621a7d75aa3b79fce7f4ea2e33c
Mike Parker <mikeparker104@users.noreply.github.com>
2024-09-06 14:42:02 +0100

    Baseline theme images sample (#1)

---
+++ ThemeImagesSample/ThemeImagesSample/Theming/ThemingAppBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.LifecycleEvents;
+
+namespace ThemeImagesSample;
+
+public static class ThemingAppBuilderExtensions
+{
+	public static MauiAppBuilder UseTheming(this MauiAppBuilder builder)
+	{
+		builder.ConfigureLifecycleEvents(lifecycle =>
+		{
+#if IOS || MACCATALYST
+			lifecycle.AddiOS(ios =>
+			{
+				ios.FinishedLaunching((app, options) =>
+				{
+                    ThemeManager.Shared.Init();
+                    return true;
+				});
+			});
+#elif ANDROID
+			lifecycle.AddAndroid(android => android.OnCreate((activity, bundle) => ThemeManager.Shared.Init()));
+#elif WINDOWS
+			lifecycle.AddWindows(windows => windows.OnLaunched((app, args) =>  => ThemeManager.Shared.Init()));
+#endif
+		});
+
+		return builder;
+	}
+}
